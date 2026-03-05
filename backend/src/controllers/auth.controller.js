b9cc9d5982c5390e39c2d795a40cfc27a6feb960
Harshit Purwar <harshit.purwar2004@gmail.com>
2026-03-05 23:37:40 +0530

    fix cross origin auth cookie

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,60 +1,66 @@
-import { registerUser,loginUser } from "../services/auth.service.js";
+import { registerUser, loginUser } from "../services/auth.service.js";
 
 import generatetoken from "../utils/generatetoken.js";
 
-const register = async (req,res)=>{
+const register = async (req, res) => {
 
-    try{
-        const user =await registerUser(req.body);
-        const token =generatetoken(user._id);
-        res.cookie("token", token, { httpOnly: true,
+    try {
+        const user = await registerUser(req.body);
+        const token = generatetoken(user._id);
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure:true,
+            samesite:"None",
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-         });
+        });
 
         res.status(201).json({
-            message:"registered successfully",
-            user:user
+            message: "registered successfully",
+            user: user
         })
 
 
     }
-    catch(err){
+    catch (err) {
         res.status(400).json({
-            message:err.message
+            message: err.message
         })
     }
 }
 
-const login = async (req,res)=>{
-      
+const login = async (req, res) => {
+
     try {
-    const user = await loginUser(req.body);
-    const token = generatetoken(user._id);
+        const user = await loginUser(req.body);
+        const token = generatetoken(user._id);
 
-    res.cookie("token", token, { httpOnly: true ,
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-    });
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "None",
+            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        });
 
-    res.json({ message: "Login successful" });
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
+        res.json({ message: "Login successful" });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 }
 
-const logout =async(req,res)=>{
-    res.cookie("token","",{
-        httpOnly:true,
-         expires: new Date(0),
+const logout = async (req, res) => {
+    res.cookie("token", "", {
+        httpOnly: true,
+        expires: new Date(0),
     })
 
-     res.status(200).json({
-    message: "Logged out successfully",
-  });
+    res.status(200).json({
+        message: "Logged out successfully",
+    });
 }
 
 
 
-export{
+export {
     register,
     login,
     logout
