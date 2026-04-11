1fccb4e18dbade26ae6ecccc9bcc439a9ee2cf3a
Harshit Purwar <harshit.purwar2004@gmail.com>
2026-04-11 18:05:33 +0530

    fixed

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -17,32 +17,12 @@ router.get("/me", protect, getMe);
 // Google OAuth — Step 1: redirect to Google
 router.get("/google",googleAuth);
 
-// Google OAuth — Step 2: callback
-
-// router.get("/google/callback",
-//   (req, res, next) => {
-//     passport.authenticate("google", { session: false }, (err, user, info) => {
-//       console.log("=== GOOGLE CALLBACK ===");
-//       console.log("err:", err?.message);
-//       console.log("user:", user?.email);
-//       console.log("info:", info);
-
-//       if (err || !user) {
-//         console.log("Redirecting to login — err or no user");
-//         return res.redirect(`${process.env.CLIENT_URL}/login`);
-//       }
-
-//       const token = generatetoken(user._id);
-//       res.redirect(`${process.env.CLIENT_URL}/dashboard?token=${token}`);
-//     })(req, res, next);
-//   }
-// );
 
 router.get(
   "/google/callback",
   passport.authenticate("google", {
     session: false,
-    failureRedirect: `${process.env.CLINET_URI}/login?error=google_auth_failed`,
+    failureRedirect: `${process.env.CLIENT_URL}/login?error=google_auth_failed`,
   }),
   googleAuthCallback
 )
